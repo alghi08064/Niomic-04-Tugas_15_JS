b6ef7d8b405cfa1795db73c724aa032f067962a6
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-03 14:06:05 +0300

    Tugas 15 JS
    Tugas ini untuk Niomic

---
+++ Tugas15.js
@@ -0,0 +1,11 @@
+function sisimiring() {
+var ab = 8;
+var bc = 6;
+
+console.log('Nilai AB:', ab)
+console.log('Nilai BC:', bc)
+
+ac = (Math.pow(ab,2) + Math.pow(bc,2))
+return ac
+}
+console.log('Panjang sisi AC pada segitiga siku-siku tersebut adalah:',Math.sqrt(sisimiring()),'cm')
